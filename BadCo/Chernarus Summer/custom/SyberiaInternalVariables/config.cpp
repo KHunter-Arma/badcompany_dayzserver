586058a0e0a5f99a8e2fe42430fc9cd381da21ba
K.Hunter <hunterzarma@gmail.com>
2024-11-26 18:57:34 +0300

    fixes and tweaks to sleep, zvirus, combat and other misc. stuff

--- BadCo/Chernarus Summer/custom/SyberiaInternalVariables/config.cpp
+++ BadCo/Chernarus Summer/custom/SyberiaInternalVariables/config.cpp
@@ -2,8 +2,13 @@ class CfgPatches { class SyberiaInternalVariables { units[] = {}; requiredAddons
 
 class CfgSyberia {
     class SleepingSystem {
+       sleepingUnconsionEnabled = 1;
+       sleepingIncPerUnconsionBoostValue = 35;
+       sleepingIncPerUnconsionBoostTime = 60;
+       sleepingIncPerSleepingLvl1Sec = 80;
+       sleepingIncPerSleepingLvl2Sec = 160;
        sleepingHealPerSec01 = 0.0002;
-       sleepingHealInfluenzaChance = 0.33;
+       sleepingHealInfluenzaChance = 0.03;
     };
     class MedicineSystem {
        sepsisStage1TimeSec = 3600;
@@ -14,21 +19,24 @@ class CfgSyberia {
        zvirusStage1TimeSec = 10800;
        zvirusStage2TimeSec = 43200;
        zvirusDeathTimeSec = 21600;
-       zvirusBloodLosePerSec = 0.005;
-       zvirusHealthLosePerSec = 0.0025;
-       zvirusCuthitSpawnChance = 0.008;
+       zvirusBloodLosePerSec = 0.08;
+       zvirusHealthLosePerSec = 0.00625;
+       zvirusCuthitSpawnChance = 0.0001;
        zvirusPainSpawnChance = 0.0006;
        zvirusOnHitIncrementTimer = 0;
-       zvirusZombieHitChance = 0.005;
-       zvirusZombieBloodTransferChance = 0.2;
+       zvirusZombieHitChance = 0.0025;
+       zvirusZombieBloodTransferChance = 0.25;
+       zvirusAntidotHealthDmg01 = 0.075;
        painLvl1TimeSec = 600;
        painLvl2TimeSec = 1800;
        painLvl3TimeSec = 3600;
        painMaxDurationSec = 5400;
-       bleedingKnifehitZombieChance = 0.004;
+       bleedingBullethitRemoveBandagedTimeSec = -1;
+       bleedingKnifehitZombieChance = 0.0015;
        bleedingKnifehitRemoveBandagedTimeSec = -1;
        bleedingVisceraHealthlosePerSecond = 0.03;
        bleedingHematomaHealthlosePerSecond = 0.001;
+       bleedingZombieHitChance = 0;
        hematomaPlayerhandsHitChance = 0;
        hematomaZombieHitChance = 0.05;
        hematomaRegenTimerSec = 7200;
@@ -39,18 +47,23 @@ class CfgSyberia {
        visceraBullethitTorsoChance = 0.2;
        concussionRegenTimeSec = 7200;
        concussionZombieHitChance = 0.02;
+       hematopoiesisEffectBloodPerSec = 1;
        mindstateHealPerSec = 0.01;
-       mindstateEmptyDeadtimeSec = 300;
+       mindstateEmptyDeadtimeSec = 135;
        influenzaIncubatePeriodsSec[] = { 1800, 9000, 10800 };
        influenzaDeathTime[] = { 0, 0, 5400 };
        influenzaApplyOnColdWarnChance = 0.0003;
        influenzaApplyOnColdCritChance = 0.003;
        stomatchpoisonChanceDrinkWell = 0.1;
        stomatchpoisonDefaultTimes[] = { 600, 1200, 900 };
-       zombieHitDecreaseMind[] = { 0.133, 5 };
+       radiationSleepingDec = 1;
+       shockDecrementOnLowHealthAndBlood = 0.05;
+       zombieHitDecreaseMind[] = { 0.02, 5 };
        zombieKillDecreaseMind[] = { 0.1, 10 };
     };
     class AttackSystem {
+       damageGlovesOnHandsOnAttack = 0.5;
+       damageHandsOnAttackChance = 0;
     };
     class ZombieSystem {
        zombieOpenDoorEnable = 0;
@@ -65,15 +78,16 @@ class CfgSyberia {
        bookPagesCountToCut = 20;
     };
     class SkillsExperienceSystem {
+       skillsExpStealthCrouchIncrement = 12;
     };
     class WorldSystem {
        airDayTemperatureMin = 21;
-       airDayTemperatureMax = 32;
+       airDayTemperatureMax = 30;
        airNightTemperatureMin = 15;
        airNightTemperatureMax = 22;
        temperatureDrinkPondPenalty = -0.0005;
        temperatureDrinkWellPenalty = -0.01;
-       temperatureHeightReduction = 0.01;
+       temperatureHeightReduction = 0.015;
        clothingDrenchedHeatDrain = -0.5;
        clothingWetHeatDrain = -0.2;
        additionalTemperatureResistForSurvivalSkill = 30;
